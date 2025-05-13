1f9ea64eb54f60c82b16f07df65feea7270743f9
amonroca <roc23009@byui.edu>
2025-05-12 21:32:26 -0300

    Updates the display of quantity in the cart and improves the logic for adding products to the cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,22 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // get the cart from local storage
+  const items = getLocalStorage("so-cart") || [];
+  // check if the product is already in the cart
+  const existingProduct = items.find((item) => item.Id === product.Id);
+  // if the product is already in the cart, update the quantity
+  if (existingProduct) {
+    existingProduct.Quantity += 1;
+  } else {
+    // if the product is not in the cart, add it
+    product.Quantity = 1;
+    items.push(product);
+  }
+  setLocalStorage("so-cart", items);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
