06fe52c587f35ef292de8a79202679ea71762fee
amonroca <roc23009@byui.edu>
2025-05-21 09:13:57 -0300

    Adds discount and old price display in product details and product list

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,13 +1,27 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
+  // Check if the product is discounted
+  const isDiscounted = product.FinalPrice < product.SuggestedRetailPrice;
+  // Calculate the discount percentage
+  // Use Math.round to round the discount percentage to the nearest integer
+  const discountPercent = isDiscounted
+    ? Math.round(((product.SuggestedRetailPrice - product.FinalPrice) / product.SuggestedRetailPrice) * 100)
+    : 0;
+
+  // Return the HTML string for the product card
+  // Use template literals for better readability
   return `
     <li class="product-card">
-      <a href="product_pages/?products=${product.Id}">
+      <a href="product_pages/?product=${product.Id}">
         <img src="${product.Image}" alt="${product.Name}">
-        <h2>${product.Brand.Name}NOOOOO</h2>
-        <h3>${product.Name}</h3>
-        <p class="product-card__price">$${product.FinalPrice}</p>
+        <h3>${product.Brand.Name}</h3>
+        <h2>${product.Name}</h2>
+        ${isDiscounted ? `<p class="product-card__old-price"><small>from:<s>$${product.SuggestedRetailPrice}</s></small></p>` : ""}
+        <p class="product-card__price">
+          $${product.FinalPrice}
+          ${isDiscounted ? `<span class="discount-badge">-${discountPercent}%</span>` : ""}
+        </p>
       </a>
     </li>
     `;
@@ -26,10 +40,11 @@ export default class ProductList {
   }
 
   renderList(list) {
-    // const htmlStrings = list.map(productCardTemplate);
-    // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
-
+    // Filter out specific products by their Id
+    // This filtering logic meets the requirement of not showing specific products by now
+    // Most lilkely, this will be refactored in the future when we can show all products
+    const filteredList = list.filter(product => product.Id !== "989CG" && product.Id !== "880RT");
     // apply use new utility function instead of the commented code above
-    renderListWithTemplate(productCardTemplate, this.listElement, list);
+    renderListWithTemplate(productCardTemplate, this.listElement, filteredList);
   }
 }
